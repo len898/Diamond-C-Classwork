472ef2b032f87c88b9f6f918ce4a87c873899035
lennart@familie-richter.de <lennart@familie-richter.de>
2023-02-21 18:21:28 -0800

    Finished Diamond, works for even and odds. Width has to be hardcoded.

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,44 +1,59 @@
 #include <iostream>
 
-//void fillRow(char arr1[], int length, char background);
-
 int main() {
-	const int width = 21;
-	//std::cout << "Enter the width: ";
-	//std::cin >> width;
+	const int width = 80;
 
-	char arrayToBeUsed[width][width];
+	char arrayToBeUsed[width][width] ;
 
 	for (int row = 0; row < width; row++) {
 		for (int column = 0; column < width; column++) {
 			arrayToBeUsed[row][column] = ' ';
 		}
 	}
-	int offset = width / 2;
-	int distFromMiddle = width - 1;
 
-	for (int row = 0; row < width/2; row++) {
-		int numSpecial = row * 2 + 1;
-		int currStars = 0;
-		for (int column = offset; column < width; column++) {
-			arrayToBeUsed[row][column] = '*';
-			arrayToBeUsed[row + distFromMiddle][column] = '*';
-			currStars++;
-			if (currStars == numSpecial)
-			{
-				column = width;
+	if (width % 2 != 0) {
+		int starsForRow = width;
+		int middle = width / 2;
+		int offset = 0;
+		int middleDistance = 0;
+
+		while (starsForRow >= 1) {
+			for (int numStars = 0; numStars < starsForRow; numStars++) {
+				arrayToBeUsed[middle + offset][middleDistance + numStars] = '*';
+				arrayToBeUsed[middle - offset][middleDistance + numStars] = '*';
 			}
-		}
-		offset--;
-		distFromMiddle = distFromMiddle - 2;
-	}
-
-	for (int row = width / 2; row == width / 2; row++) {
-		for (int column = 0; column < width; column++) {
-			arrayToBeUsed[row][column] = '*';
+			starsForRow -= 2;
+			offset += 1;
+			middleDistance += 1;
 		}
 	}
+	else {
+		int starsForRow = width;
+		int middle = width / 2;
+		int lowerOffset = 1;
+		int upperOffset = 0;
+		int middleDistance = 0;
 
+		while (starsForRow >= 1) {
+			for (int numStars = 0; numStars < starsForRow; numStars++) {
+				if (middleDistance + numStars < width / 2) {
+					arrayToBeUsed[middle + upperOffset][middleDistance + numStars] = '\\';
+					arrayToBeUsed[middle - lowerOffset][middleDistance + numStars] = '/';
+				}
+				else {
+					arrayToBeUsed[middle + upperOffset][middleDistance + numStars] = '/';
+					arrayToBeUsed[middle - lowerOffset][middleDistance + numStars] = '\\';
+				}
+				
+			}
+			starsForRow -= 2;
+			upperOffset += 1;
+			lowerOffset += 1;
+			middleDistance += 1;
+		}
+	}
+
+	
 
 	for (int row = 0; row < width; row++) {
 		for (int column = 0; column < width; column++) {
